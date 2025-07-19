5a0f506cb9096d41bad50777a8f6a93b3f28512e
haroonkpk <haroonkhanlala47@gmail.com>
2025-07-19 07:29:33 +0500

    feat: configure production static file serving and update axios base URL logic

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,8 +8,11 @@ import messageRoutes from "./routes/message.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
+import path from "path";
 
 dotenv.config();
+const __dirname = path.resolve();
+
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 
@@ -24,6 +27,14 @@ app.use("/api/auth", authRoute);
 app.use("/api/profile", profileRoutes);
 app.use("/api/request", collabRequestRoutes);
 app.use("/api/message", messageRoutes);
+ 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(3000, () => {
   console.log("Server is running on port 3000");
