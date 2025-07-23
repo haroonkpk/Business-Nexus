739c252f5d7b41de5e6251fb617f4bf237d489a0
haroonkpk <haroonkhanlala47@gmail.com>
2025-07-23 13:17:16 +0500

    fix: update axios base URL for development environment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://localhost:3000/api"
+      ? "http://localhost:5001/api"
       : "/api",
   withCredentials: true,
 });
